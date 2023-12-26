8b029fc84a895441fa97761bac5df81280091b85
JamesLawwd <jleo56291@gmail.com>
2023-12-26 15:08:24 +0300

    initial commit

--- App/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'  # Adjust the database URI as needed
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
